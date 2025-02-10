1d499ccf9d0e404aa6a78275b1ddbf8a8bc00154
Ubuntu <ubuntu@ip-172-31-18-212.ap-southeast-1.compute.internal>
2025-02-10 10:40:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, AWS CI/CD!"
+    return "Hello, AWS CI/CD updated for test!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
