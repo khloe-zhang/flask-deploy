f024b2d3413c9ba3513ef398e423b0aba83fe873
yushan <zhangyushan8327@outlook.com>
2025-02-15 18:23:34 +0800

    Add a database table

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # 创建数据库对象
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    name = db.Column(db.String(80), unique=True, nullable=False) # 姓名
+    email = db.Column(db.String(120), unique=True, nullable=False) # 邮箱
+
+with app.app_context():
+    # 创建表
+    db.create_all()
+
 @app.route("/")
 def home():
     return "Hello, AWS CI/CD updated on Feb 15! 🚀 Now with Auto Deployment! Now Flask is connected to MySQL!"
